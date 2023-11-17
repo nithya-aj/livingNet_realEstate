0ede44307067997e40b1d5bfcd91c5751e66a7c5
Nithya AJ <aj7nithya@gmail.com>
2023-11-18 00:10:18 +0530

    Added create residency endpoint and the associated controller logic.

--- server/controllers/residencyController.js
+++ server/controllers/residencyController.js
@@ -2,5 +2,38 @@ import asyncHandler from "express-async-handler";
 import { prisma } from "../config/prismaConfig.js";
 
 export const createResidency = asyncHandler(async (req, res) => {
-  console.log("endpoint created");
+  console.log("residency create endpoint");
+  const {
+    title,
+    description,
+    price,
+    address,
+    city,
+    country,
+    facilities,
+    image,
+    userEmail,
+  } = req.body.data;
+  console.log(req.body.data);
+  try {
+    const residency = await prisma.residency.create({
+      data: {
+        title,
+        description,
+        price,
+        address,
+        city,
+        country,
+        facilities,
+        image,
+        owner: { connect: { email: userEmail } },
+      },
+    });
+    res.send({ message: "Residency created!", residency });
+  } catch (error) {
+    if (error.code === "P2002") {
+      throw new Error("A residency with address already there");
+    }
+    throw new Error(error.message);
+  }
 });
