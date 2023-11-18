ee09390bc889fc89557430a9ac595201c74c47e1
Nithya AJ <aj7nithya@gmail.com>
2023-11-18 19:31:27 +0530

    Added functions to create, get all, and get a specific residency.

--- server/controllers/residencyController.js
+++ server/controllers/residencyController.js
@@ -1,6 +1,7 @@
 import asyncHandler from "express-async-handler";
 import { prisma } from "../config/prismaConfig.js";
 
+// function to create residency
 export const createResidency = asyncHandler(async (req, res) => {
   console.log("residency create endpoint");
   const {
@@ -35,5 +36,28 @@ export const createResidency = asyncHandler(async (req, res) => {
       throw new Error("A residency with address already there");
     }
     throw new Error(error.message);
+  }
+});
+
+// function to get all residencies
+export const getAllResidencies = asyncHandler(async (req, res) => {
+  const residencies = await prisma.residency.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  res.send(residencies);
+});
+
+// function to get a specific residency
+export const getResidency = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const residency = await prisma.residency.findUnique({
+      where: { id },
+    });
+    res.send(residency);
+  } catch (error) {
+    throw new Error(err.message);
   }
 });
--- server/routes/residencyRoute.js
+++ server/routes/residencyRoute.js
@@ -1,7 +1,12 @@
 import express from "express";
-import { createResidency } from "../controllers/residencyController.js";
+import {
+  createResidency,
+  getAllResidencies,
+  getResidency,
+} from "../controllers/residencyController.js";
 const router = express.Router();
 
 router.post("/create", createResidency);
-
+router.get("/all-residencies", getAllResidencies);
+router.get("/:id", getResidency)
 export { router as residencyRoute };
