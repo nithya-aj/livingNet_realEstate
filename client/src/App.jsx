2620120a2cf166d69e080e9a8366849ee1cafabf
Nithya AJ <aj7nithya@gmail.com>
2023-11-19 22:26:16 +0530

    Update dependencies, add react-router-dom and react-router, update App component and add Layout component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
 import Website from "./pages/Website";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Suspense } from "react";
+import Layout from "./components/Layout/Layout";
+import Properties from "./pages/Properties/Properties";
 
 function App() {
-  return <Website />;
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Website />} />
+            <Route path="/properties" element={<Properties />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
 }
 
 export default App;
