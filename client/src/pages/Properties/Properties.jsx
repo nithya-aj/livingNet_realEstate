4ee585cc59d767a4d585d009651a21b6cfcaf2c5
Nithya AJ <aj7nithya@gmail.com>
2023-11-20 15:01:47 +0530

    Update dependencies, add lodash, react-spinners, and update property card and residencies components

--- client/src/pages/Properties/Properties.jsx
+++ client/src/pages/Properties/Properties.jsx
@@ -2,14 +2,40 @@ import React from "react";
 import "./Properties.css";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import useProperties from "../../hooks/useProperties";
+import { PuffLoader } from "react-spinners";
+import PropertyCard from "../../components/PropertyCard/PropertyCard";
 
 const Properties = () => {
   const { data, isError, isLoading } = useProperties();
-  console.log(data, "data");
+  if (isError) {
+    return (
+      <div>
+        <span>Wrrpr while loading...</span>
+      </div>
+    );
+  }
+  if (isLoading) {
+    return (
+      <div className="wrapper flexCenter" style={{ height: "60vh" }}>
+        <PuffLoader
+          height="80"
+          width="80"
+          radius={1}
+          color="#4066ff"
+          aria-label="puff-loading"
+        />
+      </div>
+    );
+  }
   return (
     <div className="wrapper">
       <div className="flexColCenter paddings innerWidth properties-container">
         <SearchBar />
+        <div className="paddings flexCenter properties">
+          {data.map((card, i) => (
+            <PropertyCard card={card} key={i} />
+          ))}
+        </div>
       </div>
     </div>
   );
